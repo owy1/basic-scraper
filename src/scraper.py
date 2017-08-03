393ed2d382c9075704d60428382baf93ca41e9fe
Ophelia Yin <owyowy1@gmail.com>
2017-08-02 18:50:18 -0700

    step2

--- src/scraper.py
+++ src/scraper.py
@@ -1,9 +1,14 @@
 """Learning to web scrap."""
 from bs4 import BeautifulSoup
+from operator import itemgetter, attrgetter
 import requests
 import sys
 import os
 import re
+import json
+import geocoder
+import argparse
+import pprint
 
 
 __location__ = os.path.join(os.path.dirname(__file__), 'inspection_page.html')
@@ -22,7 +27,7 @@ INSPECTION_PARAMS = {
     'Inspection_Type': 'All',
     'Inspection_Start': '',
     'Inspection_End': '',
-    'Inspection_Closed_Establishment': 'A',
+    'Inspection_Closed_Business': 'A',
     'Violation_Points': '',
     'Violation_Red_Points': '',
     'Violation_Descr': '',
@@ -31,7 +36,7 @@ INSPECTION_PARAMS = {
 }
 
 
-def get_inpsection_page(**kwargs):
+def get_inspection_page(**kwargs):
     """requests.get(url) doesn't work."""
     url = INSPECTION_DOMAIN + INSPECTION_PATH
     params = INSPECTION_PARAMS.copy()
@@ -41,10 +46,12 @@ def get_inpsection_page(**kwargs):
     # import pdb; pdb.set_trace()
     resp = requests.get(url, params=params)
     resp.raise_for_status()
+    # with open('inspection_page.html', "w") as fo:
+    #     fo.write(resp.content.decode("utf-8"))
     return resp.content, resp.encoding
 
 
-def load_inpsection_page():
+def load_inspection_page():
     """Load inspection_page.html in src directory."""
     with open(__location__, 'r') as f:
         content = f.read()
@@ -128,32 +135,117 @@ def extract_score_data(elem):
     data = {
         u'Average Score': average,
         u'High Score': high_score,
-        u'Total Inspecitons': samples
+        u'Total Inspections': samples
     }
     return data
 
 
-if __name__ == '__main__':  # pragma: no cover
+def generate_results(sort='High Score', count=5, reverse=False, test=False):
+    """."""
     kwargs = {
         'Inspection_Start': '2/1/2017',
         'Inspection_End': '6/1/2017',
         'Zip_Code': '98109'
     }
-    if len(sys.argv) > 1 and sys.argv[1] == 'test':
-        html, encoding = load_inpsection_page()
+    if test:
+        html, encoding = load_inspection_page()
     else:
-        html, encoding = get_inpsection_page(**kwargs)
+        html, encoding = get_inspection_page(**kwargs)
     doc = parse_source(html, encoding)
     listings = extract_data_listings(doc)
-    dct = {}
-    for listing in listings:
+    data_list = []
+    for listing in listings[:count]:
         metadata = extract_restaurant_metadata(listing)
         score_data = extract_score_data(listing)
-        both = dict(metadata, **score_data)
-        dct.update({metadata['Business Name'][0]: both})
-        first5pairs = {k: dct[k] for k in list(dct.keys())[:5]}
-        for name, dictionary in first5pairs.items():
-            print(name)
-            for data, val in dictionary.items():
-                print(data, val)
-        print()
+        metadata.update(score_data)
+        data_list.append(metadata)
+    if sort == 'High Score':
+        sort = u'High Score'
+    if sort == 'Average Score':
+        sort = u'Average Score'
+    if sort == 'Total Inspections':
+        sort = u'Total Inspections'
+    # import pdb; pdb.set_trace()
+    try:
+        data_list = sorted(data_list, key=itemgetter(sort),
+                           reverse=False)
+    except UnboundLocalError:
+        pass
+
+    for item in data_list[:count]:
+        yield item
+
+
+def get_geojson(result):
+    """."""
+    address = " ".join(result.get('Address', ''))
+    if not address:
+        return None
+    # geocoded = geocoder.google(address)
+    # return geocoded.geojson
+    geocoded = geocoder.google(address)
+    geojson = geocoded.geojson
+    inspection_data = {}
+    use_keys = (
+        'Business Name', 'Average Score', 'Total Inspections',
+        'High Score', 'Address',
+    )
+    for key, val in result.items():
+        if key not in use_keys:
+            continue
+        if isinstance(val, list):
+            val = " ".join(val)
+        inspection_data[key] = val
+        new_address = geojson['properties'].get('address')
+        if new_address:
+            inspection_data['Address'] = new_address
+        geojson['properties'] = inspection_data
+        return geojson
+
+
+if __name__ == '__main__':  # pragma: no cover
+    # test = len(sys.argv) > 1 and sys.argv[1] == 'test'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--sort',
+                        help='select: Average Score, High Score, Total Inspections',
+                        type=str)
+    parser.add_argument('-c', '--count',
+                        help="enter number of results",
+                        type=int)
+    parser.add_argument('-r', '--reverse',
+                        help='select: True or False',
+                        type=bool)
+    args = parser.parse_args()
+
+    total_result = {'type': 'FeatureCollection', 'features': []}
+    for result in generate_results(args.sort, args.count, args.reverse):
+        geo_result = get_geojson(result)
+        pprint.pprint(geo_result)
+        total_result['features'].append(geo_result)
+    with open('my_map.json', 'w') as fh:
+        json.dump(total_result, fh)
+
+    # kwargs = {
+    #     'Inspection_Start': '2/1/2017',
+    #     'Inspection_End': '6/1/2017',
+    #     'Zip_Code': '98109'
+    # }
+    # if len(sys.argv) > 1 and sys.argv[1] == 'test':
+    #     html, encoding = load_inpsection_page()
+    # else:
+    #     html, encoding = get_inpsection_page(**kwargs)
+    # doc = parse_source(html, encoding)
+    # listings = extract_data_listings(doc)
+    # dct = {}
+    # for listing in listings:
+    #     metadata = extract_restaurant_metadata(listing)
+    #     score_data = extract_score_data(listing)
+    #     both = dict(metadata, **score_data)
+    #     dct.update({metadata['Business Name'][0]: both})
+    #     first5pairs = {k: dct[k] for k in list(dct.keys())[:5]}
+    #     for name, dictionary in first5pairs.items():
+    #         print(name)
+    #         for data, val in dictionary.items():
+    #             print(data, val)
+    #     print()
